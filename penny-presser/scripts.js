a38e0c67baa4dcba860e1dec2e034523c5dd9073
pakj190 <pakj190@newschool.edu>
2025-02-27 11:19:33 -0500

    Add files via upload

---
+++ penny-presser/scripts.js
@@ -0,0 +1,21 @@
+// get the press button
+var pressButton = document.querySelector ('button[name="press"]');
+var pennySelect = document.querySelector ('select[name="penny"]');
+var pennyImage = document.querySelector('.penny');
+
+// log press button to the console
+console.log(pressButton);
+
+// add event listern to press button
+pressButton.addEventListener('click', () => {
+
+    // log the penny value
+    console.log(pennySelect.value);
+
+    // log the penny image
+    console.log(pennyImage);
+
+    // set the penny image src
+    
+
+});
