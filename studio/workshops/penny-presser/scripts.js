89ae63034b8c0f895f94688f44d1b30f7709ba2b
pakj190 <pakj190@newschool.edu>
2025-03-06 09:56:30 -0500

    studio workshop

--- studio/workshops/penny-presser/scripts.js
+++ studio/workshops/penny-presser/scripts.js
@@ -1,5 +1,6 @@
 // get the press button
 var pressButton = document.querySelector ('button[name="press"]');
+var studentSelect = document.querySelector('select[name="student"]');
 var pennySelect = document.querySelector ('select[name="penny"]');
 var pennyImage = document.querySelector('.penny');
 
@@ -9,6 +10,9 @@ console.log(pressButton);
 // add event listern to press button
 pressButton.addEventListener('click', () => {
 
+    // log the student value
+    console.log(studentSelect.value);
+
     // log the penny value
     console.log(pennySelect.value);
 
@@ -16,6 +20,7 @@ pressButton.addEventListener('click', () => {
     console.log(pennyImage);
 
     // set the penny image src
-    pennyImage.src = 'img/' + pennySelect.value;
+   //  pennyImage.src = 'img/' + pennySelect.value;
+   pennyImage.src = 'https://' + studentSelect.value + '.interactive.rodeo/studio/workshops/penny-presser/img/' + pennySelect.value;
     
 });
