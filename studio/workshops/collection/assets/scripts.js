f78969ad7d5a29308a64c70c60ad7b05f690f1c7
pakj190 <pakj190@newschool.edu>
2025-03-20 10:47:58 -0400

    workshop js

--- studio/workshops/collection/assets/scripts.js
+++ studio/workshops/collection/assets/scripts.js
@@ -3,3 +3,16 @@ var request = await fetch('assets/entries.json')
 var entries = await request.json();
 
 console.log(entries);
+
+
+// get the input elements
+var locationSelect = document.querySelector('select[name="location"]');
+var colorSelect = document.querySelector('select[name="color"]');
+var subjectSelect = document.querySelector('select[name="subject"]');
+
+console.log(locationSelect, colorSelect, subjectSelect);
+
+// get the submit button
+var submitButton = document.querySelector('button[name="submit"]');
+
+console.log(submitButton);
